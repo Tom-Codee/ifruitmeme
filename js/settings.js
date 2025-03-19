ebdab58cf277cf1ada97c2c76939dff9afd6c82e
Tom-Codee <tomas@tomasdlcs>
2025-03-19 13:21:18 +0100

    añadí titulo contact

--- js/settings.js
+++ js/settings.js
@@ -1,11 +1,3 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const savedTheme = localStorage.getItem("selectedTheme");
-    if (savedTheme) {
-        applyTheme(savedTheme);
-    }
-});
-
-
 
 document.addEventListener("DOMContentLoaded", function () {
     const themeBoxes = document.querySelectorAll(".theme-box");
@@ -32,39 +24,58 @@ document.addEventListener("DOMContentLoaded", function () {
     function applyTheme(theme) {
         let bgImage = "";
         let screenColor = "";
-    
+        let customBarColor1 = "";
+        let customBarColor2 = "";
+
         switch (theme) {
             case "red":
                 bgImage = "url('../images/back/background-red.png')";
-                screenColor = "#FFD1D1"; // Color acorde
-                break;  
+                screenColor = "#FFD1D1";
+                customBarColor1 = "#FF9999";
+                customBarColor2 = "#CC0000";
+                break;
             case "blue":
                 bgImage = "url('../images/back/background-blue.png')";
                 screenColor = "#D1E0FF";
+                customBarColor1 = "#99CCFF";
+                customBarColor2 = "#0033CC";
                 break;
             case "green":
                 bgImage = "url('../images/back/background-green.png')";
                 screenColor = "#D1FFD1";
+                customBarColor1 = "#99FF99";
+                customBarColor2 = "#006600";
                 break;
-            case "purple":  
+            case "purple":
                 bgImage = "url('../images/back/background-purple.png')";
                 screenColor = "#E0D1FF";
+                customBarColor1 = "#CC99FF";
+                customBarColor2 = "#6600CC";
                 break;
             case "yellow":
                 bgImage = "url('../images/back/background-yellow.png')";
                 screenColor = "#FFF8D1";
+                customBarColor1 = "#FFFF99";
+                customBarColor2 = "#CCCC00";
                 break;
             default:
-                bgImage = "url('../images/background.png')"; // Fondo por defecto
+                bgImage = "url('../images/background.png')";
                 screenColor = "#F9F7EB";
+                customBarColor1 = "#ababab";
+                customBarColor2 = "#3f3f3f";
         }
-    
-        // ❌ No usar variables CSS aquí para `background-image`
+
+        // Aplicar estilos directamente (como en tu código)
         document.body.style.backgroundImage = bgImage;
         document.querySelector(".screen").style.backgroundColor = screenColor;
-    
-        // Guardar en localStorage para aplicar en futuras visitas
+
+        // Actualizar variables CSS para el custom-bar
+        document.documentElement.style.setProperty("--custom-bar-color1", customBarColor1);
+        document.documentElement.style.setProperty("--custom-bar-color2", customBarColor2);
+
+        // Guardar en localStorage
         localStorage.setItem("selectedTheme", theme);
     }
-    
 });
+
+
